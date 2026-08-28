372294d8c43ccdca0515902604a9a2deca32576a
Thomas Gooch <45251987+ThomasAurelius@users.noreply.github.com>
2026-08-28 15:13:24 -0500

    updates

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  
 };
-
+module.exports = {
+  allowedDevOrigins: ['192.168.1.194'],
+}
 export default nextConfig;
